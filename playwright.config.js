93b307f823c8100a1b68ee780176f3eec898efd1
pasilvaver <patricia.silva@xintec.cl>
2026-02-17 11:33:13 -0300

    Se Actualizan los datos del producto
    Se Actualiza los datos del producto y se termina el flujo completo, solo para un registro.

--- playwright.config.js
+++ playwright.config.js
@@ -48,7 +48,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://gestordescuentos-qa.abastible.cl/login',
-    
+    timezoneId: 'America/Santiago',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
